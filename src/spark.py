1e877bc7fc3125927643925086eccb1a0b4045ce
mickelshout <mickelshout@hotmail.com>
2024-06-03 16:32:29 +0200

    Spark set up Json test file added

--- src/spark.py
+++ src/spark.py
@@ -1,21 +1,19 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 # Create a Spark session
 spark = SparkSession.builder \
     .appName("Read JSON with Spark") \
     .getOrCreate()
 
-# Define the path to the JSON file
-json_file_path = "Users/Mick/Desktop/DIS/function.json"
+#ToDo Define the path to the JSON file
+json_file_path = ""
 
-# Read the JSON file into a DataFrame
-df = spark.read.json(json_file_path)
+# Read the JSON file into a DataFrame, specifying the schema
+df = spark.read.option('multiline', True).json(json_file_path)
 
 # Show the contents of the DataFrame
 df.show()
-
-# Optionally, print the schema of the DataFrame
-df.printSchema()
 
 # Stop the Spark session
 spark.stop()
