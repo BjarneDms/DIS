19cdc9a75b9418cd05d7e6488d9b38a0ed0dfdd2
YoavVaizman <yh.vaizman@gmail.com>
2024-06-05 20:32:31 +0200

    Network generation done en logs done?

--- src/network2.py
+++ src/network2.py
@@ -5,6 +5,9 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
+import json
+import inspect
+import os
 
 # ----------------------------------------------------------------------------------------------------------------------
 branching_factor = [1, 2, 3]                    # Branching factor is averagely 2
@@ -16,19 +19,21 @@ mean_start_node = 1000
 std_dev = 20                                    # Std_deviation for timestamp generation
 std_dev_start_node = 300
 network = []                                    # List of all the servers
-stop_condition = [i for i in range(1, 5)]       # How big the chance is that a node is the end node
-initial_branching = 4                           # How many options the user has (how many server paths exist)
+stop_condition = [i for i in range(1, 10)]      # How big the chance is that a node is the end node
+initial_branching = 4                         # How many options the user has (how many server paths exist)
 amount_of_dup = [2, 3, 4]                       # How many duplicates can exist
 amount_of_sisters = [2, 3]                      # How many sister nodes can exist
-chance_dup_nodes = [1, 2, 3, 4, 5, 6, 7]        # The chance of getting duplicate nodes
-chance_sisters_nodes = [1, 2, 3, 4, 5, 6, 7]    # The chance of getting sister nodes
-chance_random_link = [1]                        # Chance of getting a random link from a higher node going down
+chance_dup_nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]          # The chance of getting duplicate nodes
+chance_sisters_nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]      # The chance of getting sister nodes
+chance_random_link = [1, 2, 3]                          # Chance of getting a random link from a higher node going down
 dup_nodes_names = []                            # List we need to update the pred of the children of the dup nodes
 sisters_nodes_names = []                        # List we need to update the pred of the children of the sister nodes
 end_nodes = []                                  # List of nodes that do not have children
 nr_servers = 1                                  # int that keeps track of number of servers
-stop_log = [i for i in range(1, 4)]
+stop_log = [i for i in range(1, 30)]
 amount_of_logs = 5
+go_back_up = [1]
+chance_go_back_up = [1]
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -63,16 +68,16 @@ for i in range(initial_branching):
         response_time = round(np.random.normal(mean, std_dev), ndigits=2)
         for j in range(amount_of_node_dup):
             network.append(Server(name=nr_servers, pred=previous_server.name,
-                                  response_time=response_time, request_time=request_time))
+                                  response_time=abs(response_time), request_time=abs(request_time)))
 
-            successor_update(previous_server, network[len(network) - 1].name)                                                   #
+            successor_update(previous_server, network[len(network) - 1].name)                                        #
             nr_servers += 1
             dup_nodes.append(network[len(network) - 1])
             dup_nodes_names.append(network[len(network) - 1].name)
 
         network.append(Server(name=nr_servers, pred=dup_nodes_names,
-                              response_time=round(np.random.normal(mean, std_dev), ndigits=2),
-                              request_time=round(np.random.normal(mean, std_dev), ndigits=2)))
+                              response_time=abs(round(np.random.normal(mean, std_dev), ndigits=2)),
+                              request_time=abs(round(np.random.normal(mean, std_dev), ndigits=2))))
         successor_update2(dup_nodes, network[len(network) - 1].name)
 
         nr_servers += 1
@@ -81,7 +86,6 @@ for i in range(initial_branching):
         else:
             to_append_nodes.append(network[len(network) - 1])
         for nodes in dup_nodes:
-            #nodes.suc = network[len(network) - 1].name
             nodes.dup = dup_nodes_names
         dup_nodes = []
         dup_nodes_names = []
@@ -126,7 +130,6 @@ while len(to_append_nodes) != 0:
             else:
                 to_append_nodes.append(network[len(network) - 1])
             for nodes in dup_nodes:
-                #nodes.suc = network[len(network) - 1].name
                 nodes.dup = dup_nodes_names
             dup_nodes = []
             dup_nodes_names = []
@@ -228,6 +231,200 @@ def visualisation(network_list):
 visualisation(network)
 
 # ----------------------------------------------------------------------------------------------------------------------
+
+
+def make_path():
+    constructing = True
+    network_2 = copy.deepcopy(network)
+    route = [(root.name, False)]
+    next_server = random.choice(root.suc)
+    #root.suc.remove(next_server)
+    route.append((next_server, False))
+    go_back = False
+
+    while constructing:
+        if go_back:
+            if random.choice(chance_go_back_up) == 1:
+                go_back = False
+        if not go_back:
+            if type(network_2[next_server-1].suc) is int:
+                next_server2 = network_2[next_server-1].suc
+                network_2[next_server2 - 1].pred = next_server
+                network_2[next_server - 1].suc = ["go_back"]
+                next_server = next_server2
+                route.append((next_server, go_back))
+            elif (type(network_2[next_server-1].suc) is str or network_2[next_server-1].suc == ["go_back"]
+                  or network_2[next_server-1].suc == []):
+                next_server = network_2[next_server-1].pred
+                go_back = True
+                route.append((next_server, go_back))
+            else:
+                next_server2 = random.choice(network_2[next_server - 1].suc)
+                network_2[next_server2 - 1].pred = next_server
+                if type(network_2[next_server - 1].suc) is list:
+                    if network_2[next_server2 - 1].dup is None:
+                        network_2[next_server - 1].suc.remove(next_server2)
+                    else:
+                        if next_server2 in network_2[next_server2 - 1].dup:
+                            for dup in network_2[next_server2 - 1].dup:
+                                network_2[next_server - 1].suc.remove(dup)
+                        else:
+                            network_2[next_server - 1].suc.remove(next_server2)
+                else:
+                    #if network_2[next_server - 1].dup is None:
+                    network_2[next_server - 1].suc = ["go_back"]
+                next_server = next_server2
+                route.append((next_server, go_back))
+        else:
+            next_server = network_2[next_server - 1].pred
+            route.append((next_server, go_back))
+            go_back = True
+        if next_server == 0:
+            constructing = False
+
+    return route
+
+#question
+#- can it go back to one that it has already been at through another route?
+#- impliment that it sometimes does not go all the way up again, you might need another boolean for this
+
+log = []
+for j in range(5):
+    route = make_path()
+    base_time = abs(round(np.random.normal(mean_start_node, std_dev_start_node), ndigits=2))
+    for i in range(len(route)-1):
+        if i == 0:
+            log.append(("S0", f"S{route[1][0]}", base_time, "Request", j))
+        else:
+            if not route[i+1][1]:
+                random_response_time = network[route[i][0]].request_time + np.random.normal(mean, std_dev)
+                log.append((f"S{route[i][0]}", f"S{route[i+1][0]}",
+                            abs(round(base_time + random_response_time, ndigits=2)), "Request", j))
+                base_time += random_response_time
+            else:
+                if route[i][0] == len(network):
+                    random_response_time = network[len(network)-1].response_time + np.random.normal(mean, std_dev)
+                    log.append((f"S{route[i][0]}", f"S{route[i + 1][0]}",
+                                abs(round(base_time + random_response_time, ndigits=2)), "Response", j))
+                    base_time += random_response_time
+                else:
+                    random_response_time = network[route[i][0]].response_time + np.random.normal(mean, std_dev)
+                    log.append((f"S{route[i][0]}", f"S{route[i+1][0]}",
+                                abs(round(base_time + random_response_time, ndigits=2)), "Response", j))
+                    base_time += random_response_time
+
+for l in log:
+    print(l)
+
+final_sorted_log = sorted(log, key=lambda x: x[2])
+print()
+
+for l in final_sorted_log:
+    print(l)
+
+
+
+
+"""
+for i in range(len(route) - 1):                         # What request and response time you use at the end? solved?
+    if i == 0:
+        log.append((f'S{route[i].name}', f'S{route[i+1].name}', round(base_time,ndigits=2), "Request", j))
+    else:
+        log.append((f'S{route[i].name}', f'S{route[i + 1].name}',
+                    round(base_time+route[i].request_time, ndigits=2), "Request", j))
+        base_time = base_time+route[i].request_time
+
+for i in range(len(route) - 1, 0, -1):
+    log.append((f'S{route[i].name}', f'S{route[i-1].name}',
+                round(base_time+route[i].response_time, ndigits=2), "Response", j))
+    base_time = base_time+route[i].response_time
+
+final_log.append(log)
+
+final_sorted_log = []
+for log in final_log:
+    for details in log:
+        final_sorted_log.append(details)
+
+final_sorted_log = sorted(final_sorted_log, key=lambda x: x[2])
+"""
+
+"""
+while constructing:
+    #if not_first_round:
+
+    if working_back:
+        if type(network[route[len(route) - 1] - 1].suc) is list:
+            next_server = random.choice(network[route[len(route) - 1] - 1].suc)             # now i have the next server
+        else:
+            next_server = network[route[len(route) - 1] - 1].suc
+
+    if next_server in route or next_server == "Last_node":
+        route_back = route.copy()
+        working_back = True
+        while working_back:
+            print(route)
+            if hoi:
+                next_server = route_back[len(route_back)-2]
+                route_back.remove(next_server)
+            hoi = True
+            print(next_server)
+            print(f'route_back: {route_back}')
+            print(f'route: {route}')
+            route.append(next_server)
+            if len(route_back) == 2:
+                working_back = False
+                constructing = False
+                route.append(0)
+            elif random.choice(go_back_up) == 1 and (type(network[next_server-1].suc) is not int and str):
+                possible_servers = network[next_server-1].suc
+                for possible_server in possible_servers:
+                    if possible_server in route:
+                        possible_servers.remove(possible_server)
+                        print(possible_servers)
+                if len(possible_servers) != 0:
+                    working_back = False
+                    next_server = random.choice(possible_servers)
+                else:
+                    hoi = False
+                    i = 0
+                    for pred in route:
+                        if pred == next_server:
+                            next_server = route[i-1]
+                            i += 1
+                            print(f'next sercer{next_server}')
+                            break
+    else:
+        route.append(next_server)
+
+
+print(route)
+"""
+
+"""
+def go_up(go_on, next_server, route):
+    while go_on:
+        route.append(next_server.copy())
+        route[len(route) - 1].request_time = round((route[len(route) - 1].request_time +
+                                                    abs(np.random.normal(0, 10))), ndigits=2)
+        if type(next_server.suc) is int:
+            next = next_server.suc
+        else:
+            next = random.choice(next_server.suc)
+        if type(next) is not str and network[next - 1] not in route:
+            next_server = network[next - 1]
+        elif type(next) is not str:
+            if network[next - 1] in route:
+                print("hallo")
+                go_on = False
+        else:
+            stop = random.choice(stop_log)
+            if (next_server.suc == "Last_node" or stop == 1):
+                go_on = False
+                route.append(next_server.copy())
+                route[len(route) - 1].request_time = round((route[len(route) - 1].request_time +
+                                                            abs(np.random.normal(0, 10))), ndigits=2)
+
 final_log = []
 for j in range(amount_of_logs):                      # Log generation
     route = []
@@ -235,38 +432,66 @@ for j in range(amount_of_logs):                      # Log generation
     route[len(route) - 1].request_time = abs(round(np.random.normal(mean_start_node, std_dev_start_node), ndigits=2))
     next_server = network[random.choice(root.suc)-1]
     go_on = True
+    log_not_done = True
 
-    while go_on:
-        route.append(next_server.copy())
-        route[len(route) - 1].request_time = round((route[len(route) - 1].request_time +
-                                              abs(np.random.normal(0, 10))), ndigits=2)
-        if type(next_server.suc) is int:
-            next = next_server.suc
-        else:
-            next = random.choice(next_server.suc)
-        next_server = network[next-1]
-        stop = random.choice(stop_log)
-        if next_server.suc == "Last_node" or stop == 1:
-            go_on = False
-            route.append(next_server.copy())
-            route[len(route) - 1].request_time = round((route[len(route) - 1].request_time +
-                                                  abs(np.random.normal(0, 10))), ndigits=2)
-    #print()
-    #for server in route:
-        #print(server)
+    go_up(go_on, next_server, route)
 
+    dynamic_route = route.copy()
+
+    #go up
+    while log_not_done:
+        route.append(dynamic_route[len(dynamic_route)-2])
+        print(len(dynamic_route))
+        print(len(route))
+        if len(dynamic_route) > 0:
+            dynamic_route.pop(len(dynamic_route)-1)
+        if len(dynamic_route) == 1:
+            log_not_done = False
+        if random.choice(go_back_up) == 1:
+            new_servers_number = dynamic_route[len(dynamic_route)-1].suc
+            new_servers = []
+            if type(new_servers_number) is list:
+                for server in new_servers_number:
+                    new_servers.append(network[server-1])               # correct index?
+                for server in new_servers:
+                    if server in route:
+                        new_servers.remove(server)
+            else:
+                new_servers = []
+            if len(new_servers) != 0:
+                next_server = random.choice(new_servers)
+                go_on = True
+                go_up(go_on, next_server, route)
+                dynamic_route = route.copy()                    #misschien kan dit niet
+                for server in route:
+                    print(server)
+
+        #go down
+        #if #go up
+            #prep for go up
+            #go up
+
+
+
+    print()
+    for server in route:
+        print(server)
+"""
+"""
     log = []
     base_time = route[0].request_time
 
     for i in range(len(route) - 1):                         # What request and response time you use at the end? solved?
         if i == 0:
-            log.append((route[i].name, route[i+1].name, round(base_time,ndigits=2), "Request", j))
+            log.append((f'S{route[i].name}', f'S{route[i+1].name}', round(base_time,ndigits=2), "Request", j))
         else:
-            log.append((route[i].name, route[i + 1].name, round(base_time+route[i].request_time, ndigits=2), "Request", j))
+            log.append((f'S{route[i].name}', f'S{route[i + 1].name}',
+                        round(base_time+route[i].request_time, ndigits=2), "Request", j))
             base_time = base_time+route[i].request_time
 
     for i in range(len(route) - 1, 0, -1):
-        log.append((route[i].name, route[i-1].name, round(base_time+route[i].response_time, ndigits=2), "Response", j))
+        log.append((f'S{route[i].name}', f'S{route[i-1].name}',
+                    round(base_time+route[i].response_time, ndigits=2), "Response", j))
         base_time = base_time+route[i].response_time
 
     final_log.append(log)
@@ -279,6 +504,11 @@ for log in final_log:
 final_sorted_log = sorted(final_sorted_log, key=lambda x: x[2])
 
 print()
+
+data = []
+for log in final_sorted_log:
+    data.append(log)
+
 for log in final_sorted_log:
     print(log)
 
@@ -286,3 +516,26 @@ for log in final_log:                           # niet nodig uiteindelijk
     print()
     for details in log:
         print(details)
+
+
+
+print(f'hoi:{data}')
+
+
+
+
+
+formatted_data = [
+    {f"server_1": item[0], f"server_2": item[1], f"time_stamp": item[2], f"type": item[3], f"ID": item[4]}
+    for item in data
+]
+
+json_data = json.dumps(formatted_data, indent=4)
+
+# Step 3: Write the serialized function to a JSON file
+with open("function.json", "w") as json_file:
+    json_file.write(json_data)
+
+print("Function has been written to function.json")
+print(os.getcwd())
+"""
