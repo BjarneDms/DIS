16483dcddc74c37733ce36c255c1d94c522ed212
Bjarne <bjarnedaems@gmail.com>
2024-06-11 11:42:16 +0200

    Added Hash functions and combined buckets (1)

--- src/spark.py
+++ src/spark.py
@@ -1,8 +1,9 @@
 import networkx as nx
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, collect_list, udf
-from pyspark.sql.types import ArrayType, DoubleType, StringType
+from pyspark.sql.types import ArrayType, IntegerType
 from pyspark.sql import functions as F
+import math
 
 # Create a Spark session
 spark = SparkSession.builder \
@@ -22,76 +23,63 @@ grouped_df = df.groupBy("ID").agg(
     collect_list(col("time_stamp")).alias("time_stamps"),
     collect_list(col("type")).alias("types")
 )
+#Hash Functions
+#Hash 1: Total length (Unique server mentions)
+def assign_process_length_bucket(servers_1:ArrayType) -> list:
+    return [len(set(servers_1))]
+assign_process_length_bucket_udf = udf(assign_process_length_bucket, ArrayType(IntegerType()))
 
-# Define a UDF to compute time differences
-def compute_time_differences(time_stamps):
-    return [round(time_stamps[i] - time_stamps[i-1], ndigits=2) for i in range(1, len(time_stamps))]
+#Hash 2: Total time (in milliseconds)
+def compute_time_differences(time_stamps:ArrayType) -> list:
+    total_time = round(time_stamps[-1] - time_stamps[0], ndigits=2)
+    return [int((total_time // 50) + 1)]
+compute_time_differences_udf = udf(compute_time_differences, ArrayType(IntegerType()))
 
-# Register the UDF
-compute_time_differences_udf = udf(compute_time_differences, ArrayType(DoubleType()))
+#Hash 3: Variance in time (standard deviation(stddev)
+#First define a function to calculate the stddev
+def stddev(time_stamps:ArrayType) -> float:
+    length = len(time_stamps)
+    mean = sum(time_stamps) / length
+    var = sum((elem - mean) ** 2 for elem in time_stamps) / length
+    var = round(var, ndigits=2)
+    return math.sqrt(var)
 
-# Apply the UDF to calculate time differences
-result_df = grouped_df.withColumn("time_differences", compute_time_differences_udf(col("time_stamps")))
+#Define the hash function to hash to intervals of 5ms
+def time_variance_bucket(time_stamps:ArrayType) -> list:
+    return [int((stddev(time_stamps) // 5) + 1)]
+time_variance_bucket_udf = udf(time_variance_bucket, ArrayType(IntegerType()))
 
-# Compute the total time as the sum of time differences
-result_df = result_df.withColumn("total_time", F.expr("aggregate(time_differences, 0D, (acc, x) -> acc + x)"))
+# Apply Hash 1:
+df_h1 = grouped_df.withColumn("process_length_bucket", assign_process_length_bucket_udf(col("servers_1")))
 
-# Define sub-bucketing criteria based on the length of 'server_1' list
-process_length_buckets = [
-    (4, 6, "4-6"),
-    (7, 9, "7-9")
-]
+#Apply Hash 2:
+df_h2 = df_h1.withColumn("total_time_bucket", compute_time_differences_udf(col("time_stamps")))
 
-# Define a UDF to assign sub-buckets based on the length of 'server_1' list
-def assign_process_length_bucket(servers_1):
-    length = len(servers_1)
-    for lower, upper, sub_bucket_name in process_length_buckets:
-        if lower <= length <= upper:
-            return sub_bucket_name
-    return "out_of_range"
+#Apply Hash 3:
+df_h3 = df_h2.withColumn("stddev_bucket", time_variance_bucket_udf(col("time_stamps")))
 
-# Register the UDF
-assign_process_length_bucket_udf = udf(assign_process_length_bucket, StringType())
+# Combine buckets into a column
+#process length & total time
+length_time_bucket_df = df_h3.withColumn("length_time_bucket", F.concat_ws("_",  col("process_length_bucket"), col("total_time_bucket")))
 
-# Apply the UDF to assign each process to a sub-bucket
-bucketed_df = result_df.withColumn("process_length_bucket", assign_process_length_bucket_udf(col("servers_1")))
+#process length & stddev
+length_stddev_bucket_df = length_time_bucket_df.withColumn("length_stddev_bucket", F.concat_ws("_", col("process_length_bucket"), col("stddev_bucket")))
 
-# Define the bucketing criteria
-total_time_buckets = [
-    (200, 250, "200-250"),
-    (250, 300, "250-300"),
-    (300, 350, "300-350")
-]
+# The DataFrame that keeps all information per process (ID)
+all_info_df = length_stddev_bucket_df.select("ID", "servers_1", "servers_2", "time_stamps", "types", "length_time_bucket", "length_stddev_bucket")
 
-# Define a UDF to assign buckets based on total time
-def assign_total_time_bucket(total_time):
-    for lower, upper, bucket_name in total_time_buckets:
-        if lower <= total_time < upper:
-            return bucket_name
-    return "out_of_range"
-
-# Register the UDF
-assign_total_time_bucket_udf = udf(assign_total_time_bucket, StringType())
-
-# Apply the UDF to assign each process to a bucket
-bucketed_df = bucketed_df.withColumn("total_time_bucket", assign_total_time_bucket_udf(col("total_time")))
-
-# Combine the buckets into a single column for easier handling
-bucketed_df = bucketed_df.withColumn("combined_bucket", F.concat_ws("_", col("total_time_bucket"), col("process_length_bucket")))
-
-# The first DataFrame that keeps all information per process (ID)
-all_info_df = bucketed_df.select("ID", "servers_1", "servers_2", "time_stamps", "types", "time_differences", "total_time", "combined_bucket")
-
-# Show the DataFrame
-all_info_df.show(truncate=False)
-
-# Group by combined_bucket and collect the IDs
-bucket_to_ids_df = bucketed_df.groupBy("combined_bucket").agg(
+# Group by the 2 combined buckets and collect the IDs
+bucket_to_ids_df = length_stddev_bucket_df.groupBy("length_time_bucket", "length_stddev_bucket").agg(
     collect_list("ID").alias("process_ids")
 )
-
 # Show the DataFrame
-bucket_to_ids_df.show(truncate=False)
+bucket_to_ids_df.show()
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------#
+"""In this section a graph will be created between all candidate processes.
+    The nodes represent the processes
+    The edges the candidate pairs
+    The edge weigths are the Jaccard similarities"""
 
 # Create a graph
 G = nx.Graph()
