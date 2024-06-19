59ae5752f7dd4d65ec8635d22e99f15825c646ba
YoavVaizman <yh.vaizman@gmail.com>
2024-06-19 19:41:29 +0200

    begin experiment 1 en 0

--- src/experiment 1.py
+++ src/experiment 1.py
@@ -1,6 +1,7 @@
 import pickle
 from collections import defaultdict
 from itertools import groupby
+import itertools
 
 with open('copied_network.pkl', 'rb') as f:
     network = pickle.load(f)
@@ -54,6 +55,7 @@ for key, group in groupby(sorted_log, key=lambda x: x[2]):
     grouped_tuples[key] = list(group)
 
 dict_correct_merges = {i: None for i in range(len(grouped_tuples))}
+all_merges = set()
 
 for tuples in filtered_comparisons:
     i = 0
@@ -70,17 +72,38 @@ for tuples in filtered_comparisons:
             else:
                 error_found = True
         i += 1
-    #print(i)
-    #print(len(grouped_tuples[tuples[0]]))
+
+    if i == len(grouped_tuples[tuples[0]]):
+        all_merges.add((tuples[0], tuples[1]))
+
+    '''
     if i == len(grouped_tuples[tuples[0]]):
         if dict_correct_merges[tuples[0]] is None:
             dict_correct_merges[tuples[0]] = [tuples[1]]
         else:
             dict_correct_merges[tuples[0]].append(tuples[1])
+    '''
+all_part1_merges = set()
+for key in clusters.items():
+    if len(key[1]) > 1:
+        combinations = list(itertools.combinations(key[1], 2))
+        for combination in combinations:
+            all_part1_merges.add(combination)
+
+all_part1_merges = [(min(a, b), max(a, b)) for a, b in all_part1_merges]
+
 
 #for l in log_experiments:
     #print(l)
 
-print(dict_correct_merges)
+def jaccard_similarity(set1, set2):
+    intersection = set1.intersection(set2)
+    union = set1.union(set2)
+    return len(intersection) / len(union) if len(union) > 0 else 0
+
+print(all_merges)
+print(all_part1_merges)
+
+print(jaccard_similarity(all_merges, all_part1_merges))
 
 #splitten in missed en fout gegroepeerd
