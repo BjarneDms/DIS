02a341a5bd51115d754794329b22f1c493f00bd1
mickelshout <mickelshout@hotmail.com>
2024-06-20 15:43:59 +0200

    Updated part 1

--- src/Part1.py
+++ src/Part1.py
@@ -1,4 +1,3 @@
-import copy
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, collect_list, udf
 from pyspark.sql.types import ArrayType, IntegerType
@@ -7,7 +6,8 @@ import networkx as nx
 import community
 import json
 from filefunctions import stddev, observationfile, output
-import pickle
+from filefunctions import process_length, stddev
+
 
 
 # Create a Spark session
@@ -16,7 +16,7 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Define the path to the JSON file
-json_file_path = "../data/logfile.json"
+json_file_path = "../test.json"
 
 # Read the JSON file into a DataFrame, specifying the schema
 df = spark.read.option('multiline', True).json(json_file_path)
@@ -31,45 +31,63 @@ grouped_df = df.groupBy("ID").agg(
 #Hash Functions
 #Hash 1: Total length (Unique server mentions)
 def assign_process_length_bucket(from_servers:ArrayType) -> list:
-    return [len(set(from_servers))]
+    return [process_length(from_servers)]
 assign_process_length_bucket_udf = udf(assign_process_length_bucket, ArrayType(IntegerType()))
 
-#Hash 2: Total time (in milliseconds)
-def compute_time_differences(time_stamps:ArrayType) -> list:
-    total_time = round(time_stamps[-1] - time_stamps[0], ndigits=2)
-    return [int((total_time // 50) + 1)]
-compute_time_differences_udf = udf(compute_time_differences, ArrayType(IntegerType()))
+#Hash 2: Branching factor
+def branching_factor_bucket(types:ArrayType) -> list:
+    branching_factor = 0
+    for i in range(len(types)):
+        v = 0
+        if types[i] == 'Response':
+            v = 1
+        branching_factor += i*v
+    return [branching_factor]
+branching_factor_bucket_udf = udf(branching_factor_bucket, ArrayType(IntegerType()))
 
-#Hash 3: Variance in time (5ms)(standard deviation(stddev)
-def time_variance_bucket(time_stamps:ArrayType) -> list:
-    return [int((stddev(time_stamps) // 5) + 1)]
-time_variance_bucket_udf = udf(time_variance_bucket, ArrayType(IntegerType()))
+# Hash 3: variance 1 and 2
+variance_udf = udf(stddev, IntegerType())
+grouped_df = grouped_df.withColumn(colName="variance", col=variance_udf(col("time_stamps")))
+
+def variancehash_1(variance) -> int:
+    return (variance // 10) + 1
+
+def variancehash_2(variance) -> int:
+    return ((variance - 5) // 10) +1
+
+variancehash_1_udf = udf(variancehash_1, IntegerType())
+variancehash_2_udf = udf(variancehash_2, IntegerType())
 
 # Apply Hash 1:
 df_h1 = grouped_df.withColumn("process_length_bucket", assign_process_length_bucket_udf(col("from_servers")))
 
 #Apply Hash 2:
-df_h2 = df_h1.withColumn("total_time_bucket", compute_time_differences_udf(col("time_stamps")))
+df_h2 = df_h1.withColumn("branching_factor_bucket", branching_factor_bucket_udf(col("types")))
 
 #Apply Hash 3:
-df_h3 = df_h2.withColumn("stddev_bucket", time_variance_bucket_udf(col("time_stamps")))
+df_h3a = df_h2.withColumn("variance1_bucket", variancehash_1_udf(col("variance")))
+df_h3 = df_h3a.withColumn("variance2_bucket", variancehash_2_udf(col("variance")))
 
 # Combine buckets into a column
-#process length & total time
-length_time_bucket_df = df_h3.withColumn("length_time_bucket", F.concat_ws("_",  col("process_length_bucket"), col("total_time_bucket")))
-
-#process length & stddev
-length_stddev_bucket_df = length_time_bucket_df.withColumn("length_stddev_bucket", F.concat_ws("_", col("process_length_bucket"), col("stddev_bucket")))
+combined_bucket_a_df = df_h3.withColumn("combined_bucket1",
+    F.concat_ws("_",  col("process_length_bucket"),
+    col("branching_factor_bucket"), col("variance1_bucket")))
+combined_bucket_df = combined_bucket_a_df.withColumn("combined_bucket2",
+    F.concat_ws("_",  col("process_length_bucket"),
+    col("branching_factor_bucket"), col("variance2_bucket")))
 
 # The DataFrame that keeps all information per process (ID)
-all_info_df = length_stddev_bucket_df.select("ID", "from_servers", "to_servers", "time_stamps", "types", "length_time_bucket", "length_stddev_bucket")
+all_info_df = combined_bucket_df.select("ID", "from_servers", "to_servers", "time_stamps", "types", "combined_bucket1", "combined_bucket2")
 
-# Group by the 2 combined buckets and collect the IDs
-bucket_to_ids_df = length_stddev_bucket_df.groupBy("length_time_bucket", "length_stddev_bucket").agg(
-    collect_list("ID").alias("process_ids")
-)
+# Group and combine by the 2 combined buckets and collect the IDs
+bucket1_to_ids_df = combined_bucket_df.groupBy("combined_bucket1").agg(
+    collect_list("ID").alias("process_ids"))
+bucket2_to_ids_df = combined_bucket_df.groupBy("combined_bucket2").agg(
+    collect_list("ID").alias("process_ids"))
+bucket_to_ids_df = bucket1_to_ids_df.union(bucket2_to_ids_df).distinct()
 # Show the DataFrame
 bucket_to_ids_df.show()
+all_info_df.show(truncate=False)
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------#
 """
@@ -96,8 +114,6 @@ for row in bucket_to_ids_df.collect():
             servers_j = all_info_df.filter(col("ID") == process_id_2).select("to_servers").collect()[0]['to_servers']
             union = set(servers_i) | set(servers_j)
             intersection = set(servers_i) & set(servers_j)
-
-            print(servers_i)
 
             # Jaccard similarity to be assigned as weight to the edge
             jac_sim = float(len(intersection)) / len(union)
@@ -138,10 +154,6 @@ with open("../data/part1Output.json", 'w') as f:
 
 # Write observations to .txt
 observationfile(part="1", group=clusters, logfile=log)
-
-clusters_deepcopy = copy.deepcopy(clusters)
-with open('clusters1.pkl', 'wb') as f:
-    pickle.dump(clusters_deepcopy, f)
 
 # Stop the Spark session
 spark.stop()
