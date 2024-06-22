9651032e2839f1a32fbbc73a44f3f2d889dacc73
mickelshout <mickelshout@hotmail.com>
2024-06-22 15:15:48 +0200

    variance fixec in part1.2

--- src/Part1.2.py
+++ src/Part1.2.py
@@ -7,7 +7,7 @@ from pyspark.sql.types import ArrayType, IntegerType
 from pyspark.sql import functions as F
 import json
 from filefunctions import observationfile, output
-from filefunctions import process_length, stddev
+from filefunctions import process_length, stddev, reactiontime
 from collections import Counter
 import networkx as nx
 
@@ -51,18 +51,26 @@ branching_factor_bucket_udf = udf(branching_factor_bucket, IntegerType())
 
 # Hash 3: variance 1 and 2
 variance_udf = udf(stddev, IntegerType())
-info_df = info_df.withColumn(colName="variance", col=variance_udf(col("time_stamps")))
+reactiontime_udf = udf(reactiontime, ArrayType(IntegerType()))
+info_df = info_df.withColumn(colName="reactiontime", col=reactiontime_udf(col("time_stamps")))
+info_df = info_df.withColumn(colName="variance", col=variance_udf(col("reactiontime")))
 
 # Determine the size of the buckets
 stddev = info_df.select(stddev_spark("variance")).collect()[0][0]
-bucket_factor = 20
-bucket_size = int(stddev / bucket_factor)
+# print(f"std: {stddev}")
+#sum_std = info_df.agg({"variance": "sum"}).collect()[0][0]
+#count_std = info_df.count()
+#stdvar = sum_std / count_std
+#print(stdvar)
+bucket_factor = 0.0000005
+bucket_size = stddev / bucket_factor
+print(bucket_size)
 
 def variancehash_1(variance) -> int:
-    return variance // bucket_size + 1
+    return int(variance // bucket_size + 1)
 
 def variancehash_2(variance) -> int:
-    return (variance - bucket_size + 1) // bucket_size + 1
+    return int((variance - bucket_size + 1) // bucket_size + 1)
 
 variancehash_1_udf = udf(variancehash_1, IntegerType())
 variancehash_2_udf = udf(variancehash_2, IntegerType())
@@ -111,7 +119,7 @@ info_df.show(truncate=False)
 In this section we make a dictionary with the groups that should be merged
 assuming transitivity
 """
-jac_treshold = 0.5
+jac_treshold = 0.4
 '''
 merge_dict = {}
 info_collected = info_df.collect()
